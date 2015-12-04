af91d7a30b3797c3e74de41365c022898225b8a8
Matt Catellier <mattcatellier@gmail.com>
2015-12-03 19:47:19 -0800

    added a VS project to see how commit would work

---
+++ Excersises/Excersises/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace Starter
+{
+    class Program
+    {
+        static string output = "Hello World!";
+        static string name = "Matt Catellier";
+
+        public static void Main()
+        {
+            Console.WriteLine(output);
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
